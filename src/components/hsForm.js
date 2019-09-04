d45150da68953fb317683b4010adc47e00af1560
Jaakko <jaakko.ihanamaki@outlook.com>
2019-09-04 13:02:40 +0300

    Start working on forms

--- src/components/hsForm.js
+++ src/components/hsForm.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const HSForm = () => {
+
+  const [hsValues, sethsValues] = useState([])
+
+  const handleHsForm = (event) => {
+    event.preventDefault()
+  }
+
+  const handleRadioChange = (event) => {
+    setState({
+      selectedOption: event.target.value
+    })
+  }
+
   return (
     <div>
-      <p>Muuta splörötekstiä</p>
+      <form onSubmit={handleHsForm}>
+        <label>
+          <input
+            type='radio'
+            value={hsValues}
+            onChange={handleRadioChange}
+          />
+        Option 1
+        </label>
+        <label>
+          <input
+            type='radio'
+            value={hsValues}
+            onChange={handleRadioChange}
+          />
+        Option 2
+        </label>
+        <label>
+          <input
+            type='radio'
+            value={hsValues}
+            onChange={handleRadioChange}
+          />
+        Option 3
+        </label>
+      </form>
     </div>
   )
 }
