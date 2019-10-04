033889515c9fc339a5ff8e47220b4971ef3eed44
Jaakko <jaakko.ihanamaki@outlook.com>
2019-10-04 13:43:36 +0300

    Finish vocational form UI

--- src/components/vocForm.js
+++ src/components/vocForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Select, Scope } from '@rocketseat/unform'
+import { Form, Select, Scope, Input } from '@rocketseat/unform'
 
 const VocForm = () => {
 
@@ -18,24 +18,61 @@ const VocForm = () => {
   }
 
   const grades3 = [
+    {id: '1', title: '1'},
+    {id: '2', title: '2'},
     {id: '3', title: '3'}
   ]
 
   const grades5 = [
+    {id: '1', title: '1'},
+    {id: '2', title: '2'},
+    {id: '3', title: '3'},
+    {id: '4', title: '4'},
     {id: '5', title: '5'}
   ]
+
+  const validateInput = (event) => {
+    event.preventDefault()
+  }
 
   return (
     <div>
       <Form onSubmit={handleSubmit}>
-        <button onClick={selectGrading}>Valitse pisteytysjärjestelmä {activeGrading}</button>
-        <Scope path='fin'>
-          <label>Äidinkieli</label>
+        <button onClick={selectGrading}>Pisteytysjärjestelmä: {
+          gradeSys
+          ? '1-3'
+          : '1-5'
+        }</button>
+        <br />
+        <Scope path='base'>
+          <label>Viestintä- ja vuorovaikutusosaaminen</label>
           <Select name='grade' options={
-            
+            gradeSys
+            ? grades3
+            : grades5
           } />
+          <br />
+          <label>Matemaattis-luonnontieteellinen osaaminen</label>
+          <Select name='grade' options={
+            gradeSys
+            ? grades3
+            : grades5
+          } />
+          <br />
+          <label>Yhteiskunta- ja työelämäosaaminen</label>
+          <Select name='grade' options={
+            gradeSys
+            ? grades3
+            : grades5
+          } />
+          <br />
         </Scope>
         <br/>
+        <Scope path='avg'>
+        <label>Tutkinnon arvosanat pilkuilla erotettuna</label>
+        <Input name='grades' type='text' onKeyPress={validateInput}/>
+        </Scope>
+        <br />
         <button type='submit'>Submit</button>
       </Form>
     </div>
